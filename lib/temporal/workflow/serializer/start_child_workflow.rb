0fadfae9bb4b83849944e30b221c79d4e92abf57
Rob Holland <rob@clearbit.com>
2020-10-19 15:11:08 +0100

    Bring serializes in-line with Temporal API.

--- lib/temporal/workflow/serializer/start_child_workflow.rb
+++ lib/temporal/workflow/serializer/start_child_workflow.rb
@@ -6,24 +6,24 @@ module Temporal
     module Serializer
       class StartChildWorkflow < Base
         def to_proto
-          Temporal::Api::Decision.new(
-            decisionType: Temporal::Api::DecisionType::StartChildWorkflowExecution,
-            startChildWorkflowExecutionDecisionAttributes:
-              Temporal::Api::StartChildWorkflowExecutionDecisionAttributes.new(
+          Temporal::Api::Decision::V1::Command.new(
+            command_type: Temporal::Api::Enums::V1::CommandType::COMMAND_TYPE_START_CHILD_WORKFLOW_EXECUTION,
+            start_child_workflow_execution_command_attributes:
+              Temporal::Api::StartChildWorkflowExecutionCommandAttributes.new(
                 namespace: object.domain,
-                workflowId: object.workflow_id.to_s,
-                workflowType: Temporal::Api::WorkflowType.new(name: object.workflow_type),
-                taskList: Temporal::Api::TaskList.new(name: object.task_list),
-                input: Temporal::Api::Payloads.new(
+                workflow_id: object.workflow_id.to_s,
+                workflow_type: Temporal::Api::WorkflowType.new(name: object.workflow_type),
+                task_queue: Temporal::Api::TaskQueue.new(name: object.task_list),
+                input: Temporal::Api::Common::V1::Payloads.new(
                   payloads: [
-                    Temporal::Api::Payload.new(
+                    Temporal::Api::Common::V1::Payload.new(
                       data: JSON.serialize(object.input)
                     )
                   ]
                 ),
-                executionStartToCloseTimeoutSeconds: object.timeouts[:execution],
-                taskStartToCloseTimeoutSeconds: object.timeouts[:task],
-                retryPolicy: serialize_retry_policy(object.retry_policy),
+                execution_start_to_close_timeout: object.timeouts[:execution],
+                task_start_to_close_timeout: object.timeouts[:task],
+                retry_policy: serialize_retry_policy(object.retry_policy),
                 header: serialize_headers(object.headers)
               )
           )
@@ -36,21 +36,20 @@ module Temporal
 
           non_retriable_errors = Array(retry_policy.non_retriable_errors).map(&:name)
           options = {
-            initialIntervalInSeconds: retry_policy.interval,
-            backoffCoefficient: retry_policy.backoff,
-            maximumIntervalInSeconds: retry_policy.max_interval,
-            maximumAttempts: retry_policy.max_attempts,
-            nonRetriableErrorReasons: non_retriable_errors,
-            expirationIntervalInSeconds: retry_policy.expiration_interval
+            initial_interval: retry_policy.interval,
+            backoff_coefficient: retry_policy.backoff,
+            maximum_interval: retry_policy.max_interval,
+            maximum_attempts: retry_policy.max_attempts,
+            non_retriable_error_reasons: non_retriable_errors,
           }.compact
 
-          Temporal::Api::RetryPolicy.new(options)
+          Temporal::Api::Common::V1::RetryPolicy.new(options)
         end
 
         def serialize_headers(headers)
           return unless headers
 
-          Temporal::Api::Header.new(fields: object.headers)
+          Temporal::Api::Common::V1::Header.new(fields: object.headers)
         end
       end
     end
