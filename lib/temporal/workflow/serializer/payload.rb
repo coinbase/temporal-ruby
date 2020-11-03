d24cac9239fb2482e4a500a9ca282b08a4aba87f
Anthony Dmitriyev <antstorm@gmail.com>
2020-11-03 15:49:32 +0000

    Add encoding field to Payload metadata

--- lib/temporal/workflow/serializer/payload.rb
+++ lib/temporal/workflow/serializer/payload.rb
@@ -5,10 +5,13 @@ module Temporal
   class Workflow
     module Serializer
       class Payload < Base
+        JSON_ENCODING = 'json/plain'.freeze
+
         def to_proto
           Temporal::Api::Common::V1::Payloads.new(
             payloads: [
               Temporal::Api::Common::V1::Payload.new(
+                metadata: { 'encoding' => JSON_ENCODING },
                 data: JSON.serialize(object)
               )
             ]
