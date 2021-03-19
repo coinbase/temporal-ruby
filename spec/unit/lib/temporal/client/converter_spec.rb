e8b68a2cbbc81f0a99e793ea9243ba12a5b53840
Rob Holland <rob@opus-grove.com>
2021-03-19 15:25:10 +0000

    Add mixed payloads test case.

--- spec/unit/lib/temporal/client/converter_spec.rb
+++ spec/unit/lib/temporal/client/converter_spec.rb
@@ -63,5 +63,27 @@ describe Temporal::Client do
         expect(subject.from_payload(payload).encoding).to eq(Encoding::ASCII_8BIT)
       end
     end
+
+    describe 'for multiple payloads' do
+      it 'returns an array with the right data' do
+        payloads = Temporal::Api::Common::V1::Payloads.new(
+          payloads: [
+            Temporal::Api::Common::V1::Payload.new(
+              metadata: { 'encoding' => 'binary/plain' },
+              data: 'test'.b
+            ),
+            Temporal::Api::Common::V1::Payload.new(
+              metadata: { 'encoding' => 'json/plain' },
+              data: '{"test":{"foo":2}}'
+            ),
+          ]
+        )
+
+        expect(subject.from_payloads(payloads)).to eq([
+          'test'.b,
+          { 'test' => { 'foo' => 2 } }
+        ])
+      end
+    end
   end
 end
