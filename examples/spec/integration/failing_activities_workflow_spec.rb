197be065979ce7d3591d13ab42c89b343863450b
Jeff Schoner <jeffschoner@stripe.com>
2021-03-28 21:41:00 -0700

    Add .fail method to futures for calling a block upon workflow/activity failure

--- examples/spec/integration/failing_activities_workflow_spec.rb
+++ examples/spec/integration/failing_activities_workflow_spec.rb
@@ -7,10 +7,15 @@ describe FailingActivitiesWorkflow do
     it 'counts are consistent with 2 as the right guess and 0 a terminal failure' do
       expect(FailingActivitiesWorkflow.execute_locally(count))
         .to match(
-          finished: a_value == count,
+          finished: count,
           successes: ['two'],
           terminal_guesses: 1,
-          wrong_guesses: 4
+          wrong_guesses: 4,
+          success_callbacks: { 'two' => Set.new([2]) },
+          fail_callbacks: {
+            GuessActivity::TerminalGuess => Set.new([0]),
+            GuessActivity::WrongGuess => Set.new([1, 3, 4, 5]),
+          }
         )
     end
   end
