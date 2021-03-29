de3f526c45a87f96f7993fe562e52ad3a0c73072
Jeff Schoner <jeffschoner@stripe.com>
2021-03-28 21:41:00 -0700

    Add .fail method to futures for calling a block upon workflow/activity failure

--- examples/workflows/failing_activities_workflow.rb
+++ examples/workflows/failing_activities_workflow.rb
@@ -2,7 +2,21 @@ require 'activities/guess_activity'
 
 class FailingActivitiesWorkflow < Temporal::Workflow
   def execute(count)
-    futures = (0..count - 1).map { |i| GuessActivity.execute(i) }
+    fail_callbacks = {}
+    success_callbacks = {}
+    futures = (0..count - 1).map do |i|
+      f = GuessActivity.execute(i)
+
+      f.done do |result|
+        success_callbacks[result] = (success_callbacks[result] || Set.new).add(i)
+      end
+
+      f.failed do |exception|
+        fail_callbacks[exception.class] = (fail_callbacks[exception.class] || Set.new).add(i)
+      end
+
+      f
+    end
     workflow.wait_for_all(*futures)
 
     logger.info("#{futures.count(&:failed?)} activites of #{count} failed")
@@ -11,7 +25,9 @@ class FailingActivitiesWorkflow < Temporal::Workflow
       finished: futures.count(&:finished?),
       successes: futures.select(&:ready?).map(&:get),
       terminal_guesses: futures.count { |f| f.get.class == GuessActivity::TerminalGuess },
-      wrong_guesses: futures.count { |f| f.get.class == GuessActivity::WrongGuess }
+      wrong_guesses: futures.count { |f| f.get.class == GuessActivity::WrongGuess },
+      success_callbacks: success_callbacks,
+      fail_callbacks: fail_callbacks
     }
   end
 end
