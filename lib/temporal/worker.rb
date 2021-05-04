85feb0a44a821be19bcbbbbbfdb54cdc19a3206f
Rob Holland <rob@opus-grove.com>
2021-05-04 13:41:50 +0100

    Revert "Allow configuring the drain grace period."
    This reverts commit b051b5397cec636720b06e7615612f1467b26cf4.

--- lib/temporal/worker.rb
+++ lib/temporal/worker.rb
@@ -7,11 +7,9 @@ require 'temporal/middleware/entry'
 
 module Temporal
   class Worker
-    DEFAULT_SHUTDOWN_GRACE_PERIOD = 25 # seconds
-
     # activity_thread_pool_size: number of threads that the poller can use to run activities.
     #   can be set to 1 if you want no paralellism in your activities, at the cost of throughput.
-    def initialize(activity_thread_pool_size: Temporal::Activity::Poller::DEFAULT_OPTIONS[:thread_pool_size], shutdown_grace_period: DEFAULT_SHUTDOWN_GRACE_PERIOD)
+    def initialize(activity_thread_pool_size: Temporal::Activity::Poller::DEFAULT_OPTIONS[:thread_pool_size])
       @workflows = Hash.new { |hash, key| hash[key] = ExecutableLookup.new }
       @activities = Hash.new { |hash, key| hash[key] = ExecutableLookup.new }
       @pollers = []
@@ -21,7 +19,6 @@ module Temporal
       @activity_poller_options = {
         thread_pool_size: activity_thread_pool_size,
       }
-      @shutdown_grace_period = shutdown_grace_period
     end
 
     def register_workflow(workflow_class, options = {})
@@ -70,7 +67,7 @@ module Temporal
         pollers.each(&:stop_polling)
         # allow workers to drain in-transit tasks.
         # https://github.com/temporalio/temporal/issues/1058
-        sleep @shutdown_grace_period
+        sleep 1
         pollers.each(&:cancel_pending_requests)
         pollers.each(&:wait)
       end.join
