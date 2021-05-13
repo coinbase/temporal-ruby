fdaf87a07f14e6071aaaedb59ffa0a7ebc8b5c66
Anthony Dmitriyev <antstorm@gmail.com>
2021-05-13 23:44:54 +0100

    Rename spec

--- examples/spec/integration/serial_hello_world_workflow_spec.rb
+++ examples/spec/integration/serial_hello_world_workflow_spec.rb
@@ -1,7 +1,7 @@
 require 'workflows/serial_hello_world_workflow'
 
 describe SerialHelloWorldWorkflow, :integration do
-  it 'works' do
+  it 'completes' do
     result = run_workflow(described_class, 'Alice', 'Bob', 'John')
 
     expect(result.history.events.first.event_type).to eq(:EVENT_TYPE_WORKFLOW_EXECUTION_COMPLETED)
