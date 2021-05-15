f9de75533433aef082c4e684dc8088bc0e29ecbf
Drew Hoskins <drewhoskins@stripe.com>
2021-05-14 17:29:21 -0700

    Faster test stubbing sleep

--- spec/unit/lib/temporal/client/retryer.rb
+++ spec/unit/lib/temporal/client/retryer.rb
@@ -4,22 +4,25 @@ require 'time'
 
 describe Temporal::Client::Retryer do
   it 'backs off and stops retrying eventually' do
-    timestamps = []
-    max_wait = 2.0
+    sleep_amounts = []
+    max_wait = 6.0
+    expect(described_class).to receive(:sleep).at_least(10).times do |amount|
+      sleep_amounts << amount
+    end
+
     expect do
       result = described_class.retry_for(max_wait, retry_message: "Still trying", metadata_hash: {}) do
-        timestamps << Time.now.to_f
         raise 'try again'
       end
     end.to raise_error(StandardError)
-    epsilon = 0.1 # fudge factor for execution time, which isn't tracked in Retryer.
-    expect(timestamps.last - timestamps.first).to be <= max_wait + epsilon
+    expect(sleep_amounts.sum).to be <= max_wait
 
     # Test backoff
     initial_interval = 0.2
     backoff = 1.2
-    (0..timestamps.length - 2).each do |i|
-      expect(timestamps[i + 1] - timestamps[i]).to be >= initial_interval * (backoff**i)
+
+    sleep_amounts.each_with_index do |sleep_amount, i|
+      expect(sleep_amount).to be_within(0.01).of(initial_interval * (backoff**i))
     end
   end
 
