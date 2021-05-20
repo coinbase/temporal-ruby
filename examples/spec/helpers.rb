1124d07857e4a86b1b7ce557862de9b28af80653
Rob Holland <rob@opus-grove.com>
2021-05-20 15:14:46 +0100

    Add an integration spec for crypt converter.
    This confirms that inputs/results are encrypted and that we can get the
    plaintext result with the configured converter.

--- examples/spec/helpers.rb
+++ examples/spec/helpers.rb
@@ -9,15 +9,27 @@ module Helpers
       **args.merge(options: { workflow_id: workflow_id })
     )
 
+    return workflow_id, run_id
+  end
+
+  def wait_for_workflow_completion(workflow_id, run_id)
+    fetch_history(
+      workflow_id,
+      run_id,
+      wait_for_new_event: true,
+      event_type: :close,
+    )
+  end
+
+  def fetch_history(workflow_id, run_id, options = {})
     client = Temporal.send(:client)
 
     result = client.get_workflow_execution_history(
-      namespace: Temporal.configuration.namespace,
-      workflow_id: workflow_id,
-      run_id: run_id,
-      next_page_token: nil,
-      wait_for_new_event: true,
-      event_type: :close
+      {
+        namespace: Temporal.configuration.namespace,
+        workflow_id: workflow_id,
+        run_id: run_id,
+      }.merge(options)
     )
   end
 end
