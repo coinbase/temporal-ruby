edd86bae17eb70e7e4ddab10e8cdeab00369206f
Anthony Dmitriyev <antstorm@gmail.com>
2021-05-20 10:10:05 +0100

    Integration specs against a running server (#55)
    * Add missing arguments to client #get_workflow_execution_history
    
    * Integration test against Temporal cluster
    
    * Add spec helpers
    
    * Configure env for specs
    
    * Rename spec

--- examples/spec/integration/serial_hello_world_workflow_spec.rb
+++ examples/spec/integration/serial_hello_world_workflow_spec.rb
@@ -1,19 +1,9 @@
 require 'workflows/serial_hello_world_workflow'
 
-describe SerialHelloWorldWorkflow do
-  subject { described_class }
+describe SerialHelloWorldWorkflow, :integration do
+  it 'completes' do
+    result = run_workflow(described_class, 'Alice', 'Bob', 'John')
 
-  before { allow(HelloWorldActivity).to receive(:execute!).and_call_original }
-
-  it 'executes HelloWorldActivity' do
-    subject.execute_locally('Alice', 'Bob', 'John')
-
-    expect(HelloWorldActivity).to have_received(:execute!).with('Alice').ordered
-    expect(HelloWorldActivity).to have_received(:execute!).with('Bob').ordered
-    expect(HelloWorldActivity).to have_received(:execute!).with('John').ordered
-  end
-
-  it 'returns nil' do
-    expect(subject.execute_locally).to eq(nil)
+    expect(result.history.events.first.event_type).to eq(:EVENT_TYPE_WORKFLOW_EXECUTION_COMPLETED)
   end
 end
--- examples/spec/spec_helper.rb
+++ examples/spec/spec_helper.rb
@@ -20,6 +20,8 @@ require 'bundler'
 Bundler.require :default, :test
 
 require 'temporal/testing'
+require 'init'
+require 'helpers'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
@@ -51,6 +53,8 @@ RSpec.configure do |config|
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.include Helpers, :integration
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
