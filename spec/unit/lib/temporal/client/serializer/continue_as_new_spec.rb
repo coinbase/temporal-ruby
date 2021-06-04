61dca2bbf8dc6b6957c3f5a18041f6c402354afb
Rob Holland <rob@opus-grove.com>
2021-06-04 09:37:15 +0100

    Add failure serializer spec.
    Tidy continue_as_new spec.

--- spec/unit/lib/temporal/client/serializer/continue_as_new_spec.rb
+++ spec/unit/lib/temporal/client/serializer/continue_as_new_spec.rb
@@ -2,8 +2,6 @@ require 'temporal/client/serializer/continue_as_new'
 require 'temporal/workflow/command'
 
 describe Temporal::Client::Serializer::ContinueAsNew do
-  subject { described_class.new(payload_converters: [bytes_converter, json_converter]) }
-
   describe 'to_proto' do
     it 'produces a protobuf' do
       command = Temporal::Workflow::Command::ContinueAsNew.new(
@@ -13,12 +11,12 @@ describe Temporal::Client::Serializer::ContinueAsNew do
         timeouts: Temporal.configuration.timeouts
       )
 
-    result = described_class.new(command).to_proto
+      result = described_class.new(command).to_proto
 
-    expect(result).to be_an_instance_of(Temporal::Api::Command::V1::Command)
-    expect(result.command_type).to eql(
-      :COMMAND_TYPE_CONTINUE_AS_NEW_WORKFLOW_EXECUTION
-    )
+      expect(result).to be_an_instance_of(Temporal::Api::Command::V1::Command)
+      expect(result.command_type).to eql(
+        :COMMAND_TYPE_CONTINUE_AS_NEW_WORKFLOW_EXECUTION
+      )
     end
   end
 end
