70fc71cd07a9804988a5aedac93eab249ccdff69
Drew Hoskins <drewhoskins@stripe.com>
2021-06-07 18:25:36 -0700

    Bonus: convert existing test to use the new method

--- examples/spec/integration/serial_hello_world_workflow_spec.rb
+++ examples/spec/integration/serial_hello_world_workflow_spec.rb
@@ -4,13 +4,11 @@ describe SerialHelloWorldWorkflow, :integration do
   it 'completes' do
     workflow_id, run_id = run_workflow(described_class, 'Alice', 'Bob', 'John')
 
-    result = fetch_history(
-      workflow_id,
-      run_id,
-      wait_for_new_event: true,
-      event_type: :close,
+    result = Temporal.await_workflow_result(
+      described_class,
+      workflow_id: workflow_id,
+      run_id: run_id,
     )
-
-    expect(result.history.events.first.event_type).to eq(:EVENT_TYPE_WORKFLOW_EXECUTION_COMPLETED)
+    expect(result).to eq(nil)
   end
 end
