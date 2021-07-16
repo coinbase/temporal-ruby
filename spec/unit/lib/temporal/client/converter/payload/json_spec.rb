0f384eddfffe431ba4dd8ed69ebddd69682f10cc
Drew Hoskins <37816070+drewhoskins-stripe@users.noreply.github.com>
2021-07-16 03:48:54 -0700

    Fix float serialization roundtrip (#89)

--- spec/unit/lib/temporal/client/converter/payload/json_spec.rb
+++ spec/unit/lib/temporal/client/converter/payload/json_spec.rb
@@ -9,5 +9,12 @@ describe Temporal::Client::Converter::Payload::JSON do
 
       expect(subject.from_payload(subject.to_payload(input))).to eq(input)
     end
+
+    it 'handles floats without loss of precision' do
+      input = { 'a_float' => 1626122510001.305986623 }
+      result = subject.from_payload(subject.to_payload(input))['a_float']
+      expect(result).to be_within(1e-8).of(input['a_float'])
+    end
+
   end
 end
