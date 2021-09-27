e5115a403cc1f43ecc4ee7987092ab1301d6d3bb
Nathan Glass <nagl@stripe.com>
2021-09-27 13:17:41 -0700

    Support signal_with_start

---
+++ examples/spec/integration/signal_with_start_spec.rb
@@ -0,0 +1,49 @@
+require 'workflows/signal_with_start_workflow'
+
+describe 'signal with start' do
+
+  it 'signals at workflow start time' do
+    workflow_id = SecureRandom.uuid
+    run_id = Temporal.signal_with_start_workflow(
+      SignalWithStartWorkflow,
+      'signal_name', # the actual signal name
+      'expected value',
+      'signal_name', # the expected signal name, which the workflow takes as an arg
+      0.1, # how long to sleep for
+      options: { workflow_id: workflow_id }
+    )
+
+    result = Temporal.await_workflow_result(
+      SignalWithStartWorkflow,
+      workflow_id: workflow_id,
+      run_id: run_id,
+    )
+
+    expect(result).to eq('expected value') # the workflow should return the signal value
+  end
+
+  it 'does not launch a new workflow when signaling a running workflow through signal_with_start' do
+    workflow_id = SecureRandom.uuid
+    run_id = Temporal.signal_with_start_workflow(
+      SignalWithStartWorkflow,
+      'signal_name', # the actual signal name
+      'expected value',
+      'signal_name', # the expected signal name, which the workflow takes as an arg
+      10, # how long to sleep for
+      options: { workflow_id: workflow_id }
+    )
+
+    second_run_id = Temporal.signal_with_start_workflow(
+      SignalWithStartWorkflow,
+      'signal_name', # the actual signal name
+      'expected value',
+      'signal_name', # the expected signal name, which the workflow takes as an arg
+      0.1, # how long to sleep for
+      options: { workflow_id: workflow_id }
+    )
+
+    # If the run ids are the same, then we didn't start a new workflow
+    expect(second_run_id).to eq(run_id)
+  end
+
+end
---
+++ examples/workflows/signal_with_start_workflow.rb
@@ -0,0 +1,18 @@
+require 'activities/long_running_activity'
+
+class SignalWithStartWorkflow < Temporal::Workflow
+
+  def execute(expected_signal, sleep_for)
+    received = 'no signal received'
+
+    workflow.on_signal do |signal, input|
+      if signal == expected_signal
+        received = input
+      end
+    end
+
+    # Do something to get descheduled so the signal handler has a chance to run
+    workflow.sleep(sleep_for)
+    received
+  end
+end
