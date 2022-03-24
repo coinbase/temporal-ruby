af43a413feb6a2cb271d82f53c58a025e07108d0
Chuck Remes <chuck.remes@amount.com>
2022-03-24 15:34:44 -0500

    remove unused constant; update docs; rename method

--- lib/temporal/workflow/dispatcher.rb
+++ lib/temporal/workflow/dispatcher.rb
@@ -4,8 +4,8 @@ module Temporal
     # points to this class, #register_handler and #dispatch.
     #
     # A handler may be associated with a specific event name so when that event occurs
-    # elsewhere in the system we may dispatch the event and execute the handler. By default
-    # we *always* execute the handler associated with the event_name.
+    # elsewhere in the system we may dispatch the event and execute the handler.
+    # We *always* execute the handler associated with the event_name.
     #
     # Optionally, we may register a named handler that is triggered when an event _and
     # an optional handler_name key_ are provided. In this situation, we dispatch to both
@@ -17,14 +17,13 @@ module Temporal
       TARGET_WILDCARD = '*'.freeze
 
       EventStruct = Struct.new(:event_name, :handler)
-      DuplicateNamedHandlerRegistrationError = Class.new(StandardError)
 
       def initialize
         @handlers = Hash.new { |hash, key| hash[key] = [] }
       end
 
       def register_handler(target, event_name, handler_name: nil, &handler)
-        handlers[target] << EventStruct.new(combine(event_name, handler_name), handler)
+        handlers[target] << EventStruct.new(sanitize_handler_name(event_name, handler_name), handler)
         self
       end
 
@@ -39,7 +38,7 @@ module Temporal
       attr_reader :handlers
 
       def handlers_for(target, event_name, handler_name)
-        named_handler = combine(event_name, handler_name)
+        named_handler = sanitize_handler_name(event_name, handler_name)
 
         handlers[target]
           .concat(handlers[TARGET_WILDCARD])
@@ -53,7 +52,7 @@ module Temporal
           event_struct.event_name == WILDCARD
       end
 
-      def combine(event_name, handler_name)
+      def sanitize_handler_name(event_name, handler_name)
         return event_name unless handler_name
         "#{event_name}:#{handler_name}"
       end
