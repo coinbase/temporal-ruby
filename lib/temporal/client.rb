4a3ac1b30931a46ef0f89bc29268e49257a3b13f
Calum Webb <calum@stripe.com>
2022-04-03 22:53:07 -0700

    removed ;

--- lib/temporal/client.rb
+++ lib/temporal/client.rb
@@ -383,7 +383,7 @@ module Temporal
       end
     end
 
-    def list_closed_workflow_executions(namespace, from, to = Time.now, filter: {}, next_page_token:,)
+    def list_closed_workflow_executions(namespace, from, to = Time.now, filter: {}, next_page_token:)
       validate_filter(filter, :status, :workflow, :workflow_id)
 
       if block_given? 
