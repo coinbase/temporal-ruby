5a5732bb9d2be59839a8e02b359a8132d59b3c92
Calum Webb <calum@stripe.com>
2022-04-05 11:17:15 -0700

    connection class setup

--- lib/temporal/connection.rb
+++ lib/temporal/connection.rb
@@ -6,7 +6,7 @@ module Temporal
       grpc: Temporal::Connection::GRPC
     }.freeze
 
-    def self.generate(configuration, options: {})
+    def self.generate(configuration)
       connection_class = CLIENT_TYPES_MAP[configuration.type]
       host = configuration.host
       port = configuration.port
@@ -15,7 +15,7 @@ module Temporal
       thread_id = Thread.current.object_id
       identity = "#{thread_id}@#{hostname}"
 
-      connection_class.new(host, port, identity, options: options)
+      connection_class.new(host, port, identity, options: {max_page_size: configuration.max_page_size})
     end
   end
 end
