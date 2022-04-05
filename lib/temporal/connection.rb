46ea9eba6f75769503664f10ed384c54a25a47a4
Calum Webb <calum@stripe.com>
2022-04-05 13:32:26 -0700

    opts

--- lib/temporal/connection.rb
+++ lib/temporal/connection.rb
@@ -10,12 +10,13 @@ module Temporal
       connection_class = CLIENT_TYPES_MAP[configuration.type]
       host = configuration.host
       port = configuration.port
+      options = configuration.options
 
       hostname = `hostname`
       thread_id = Thread.current.object_id
       identity = "#{thread_id}@#{hostname}"
 
-      connection_class.new(host, port, identity, configuration.options)
+      connection_class.new(host, port, identity, options)
     end
   end
 end
