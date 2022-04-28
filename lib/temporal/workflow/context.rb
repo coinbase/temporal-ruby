2bb483bdba2672068b34fabbc23dd9fb8f9a0c85
Calum Webb <calum@stripe.com>
2022-04-28 13:20:16 -0700

    fixed

--- lib/temporal/workflow/context.rb
+++ lib/temporal/workflow/context.rb
@@ -138,8 +138,8 @@ module Temporal
           future.fail(exception)
           future.failure_callbacks.each { |callback| call_in_fiber(callback, exception) }
 
-          child_workflow_execution_future.fail(exception)
-          child_workflow_execution_future.failure_callbacks.each { |callback| call_in_fiber(callback, exception) }
+          # child_workflow_execution_future.fail(exception)
+          # child_workflow_execution_future.failure_callbacks.each { |callback| call_in_fiber(callback, exception) }
         end
 
         dispatcher.register_handler(target, 'started') do |result|
