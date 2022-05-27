9a8ed7c758c13c6fbedc9f361642498576d87849
Calum Webb <calum@stripe.com>
2022-05-26 21:30:21 -0700

    Update json_protobuf.rb

--- lib/temporal/connection/converter/payload/json_protobuf.rb
+++ lib/temporal/connection/converter/payload/json_protobuf.rb
@@ -3,6 +3,7 @@ require 'temporal/json'
 module Temporal
   module Connection
     module Converter
+      # Workflow Engine Specific: Allows decoding and encoding events from the Java SDK that uses `json/protobuf` encoding
       module Payload
         class JSONProtobuf < JSON
           ENCODING = 'json/protobuf'.freeze
