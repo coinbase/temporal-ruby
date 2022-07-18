d3c68ef4b5e32fba6dd701d0ec23db1765f00ff5
Joseph Azevedo <jazevedo@stripe.com>
2022-07-18 12:23:45 -0700

    move empty check out of Temporal::Workflow::Context::Helpers.process_search_attributes

--- lib/temporal/workflow/context_helpers.rb
+++ lib/temporal/workflow/context_helpers.rb
@@ -5,15 +5,12 @@ module Temporal
       # Shared between Context, and LocalWorkflowContext, and ExecutionOptions so we can do the same validations in test and production.
       module Helpers
 
-        def self.process_search_attributes(search_attributes, allow_empty:)
+        def self.process_search_attributes(search_attributes)
           if search_attributes.nil?
             raise ArgumentError, 'search_attributes cannot be nil'
           end
           if !search_attributes.is_a?(Hash)
             raise ArgumentError, "for search_attributes, expecting a Hash, not #{search_attributes.class}"
-          end
-          if search_attributes.empty? && !allow_empty
-            raise ArgumentError, "Cannot upsert an empty hash for search_attributes, as this would do nothing."
           end
           search_attributes.transform_values do |attribute|
             if attribute.is_a?(Time)
