37c452b2f14fc51f9abb11a238662864c3442db3
Jeff Schoner <jeffschoner@gmail.com>
2022-08-14 19:31:38 -0700

    Modify signal_with_start_workflow to cover dwillett's repro

--- examples/workflows/signal_with_start_workflow.rb
+++ examples/workflows/signal_with_start_workflow.rb
@@ -1,16 +1,20 @@
+require 'activities/hello_world_activity'
+
 class SignalWithStartWorkflow < Temporal::Workflow
 
-  def execute(expected_signal, sleep_for)
-    received = 'no signal received'
+  def execute(expected_signal)
+    initial_value = 'no signal received'
+    received = initial_value
 
     workflow.on_signal do |signal, input|
       if signal == expected_signal
+        HelloWorldActivity.execute!('expected signal')
         received = input
       end
     end
 
     # Do something to get descheduled so the signal handler has a chance to run
-    workflow.sleep(sleep_for)
+    workflow.wait_until { received != initial_value }
     received
   end
 end
