ee37874679a12be6f27d3c9c02189e62707c5444
Jeff Schoner <jeffschoner@stripe.com>
2022-09-23 17:19:37 -0700

    Add workflow task failure counter

--- lib/temporal/metrics_keys.rb
+++ lib/temporal/metrics_keys.rb
@@ -7,5 +7,6 @@ module Temporal
     WORKFLOW_POLLER_TIME_SINCE_LAST_POLL = 'workflow_poller.time_since_last_poll'.freeze
     WORKFLOW_TASK_QUEUE_TIME = 'workflow_task.queue_time'.freeze
     WORKFLOW_TASK_LATENCY = 'workflow_task.latency'.freeze
+    WORKFLOW_TASK_EXECUTION_FAILED = 'workflow_task.execution_failed'.freeze
   end
 end
