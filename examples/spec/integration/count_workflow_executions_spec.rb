89e68835d295be06239e439218fb86f13cefa7f0
Calum Webb <calum@stripe.com>
2022-11-01 15:05:23 -0700

    added specs

---
+++ examples/spec/integration/count_workflow_executions_spec.rb
@@ -0,0 +1,40 @@
+require 'workflows/count_workflow'
+require 'securerandom'
+
+describe 'Temporal.count_workflow_executions', :integration do
+  it 'counts 0 workflows when none match' do
+    result = Temporal.count_workflow_executions(
+      Temporal.configuration.namespace, 'WorkflowType="ThisDoesntExistWorkflow"'
+    )
+
+    expect(result).to eq(0)
+  end
+
+  it 'counts workflows correctly' do
+    workflow_id, run_id = run_workflow(CountWorkflow)
+
+    Temporal.await_workflow_result(
+      CountWorkflow,
+      workflow_id: workflow_id,
+      run_id: run_id
+    )
+
+    # need to loop to check result here as count_workflow_executions is eventually
+    # consistent
+    found_correct_result = false
+    count = 0
+    while count < 15 && !found_correct_result
+      result = Temporal.count_workflow_executions(
+        Temporal.configuration.namespace, "WorkflowId=\"#{workflow_id}\""
+      )
+
+      sleep 2
+
+      # should return result == 1
+      found_correct_result = result == 1
+      count += 1
+    end
+
+    expect(found_correct_result).to eq(true)
+  end
+end
