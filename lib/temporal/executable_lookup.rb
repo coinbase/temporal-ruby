14411caa1fba497befc63427c27f8f5635aefef2
Drew Hoskins <drewhoskins@stripe.com>
2022-12-12 18:25:39 -0800

    DynamicActivity

--- lib/temporal/executable_lookup.rb
+++ lib/temporal/executable_lookup.rb
@@ -1,3 +1,5 @@
+require 'temporal/errors'
+
 # This class is responsible for matching an executable (activity or workflow) name
 # to a class implementing it.
 #
@@ -11,15 +13,26 @@ module Temporal
     end
 
     def add(name, executable)
-      executables[name] = executable
+      if executable.dynamic?
+        if @fallback_executable
+          raise Temporal::TypeAlreadyRegisteredError.new(
+            "Cannot register #{name} marked as dynamic?; #{fallback_executable_name} is already registered as " \
+            "dynamic?, and there can be only one."
+          )
+        end
+        @fallback_executable = executable
+        @fallback_executable_name = name
+      else
+        executables[name] = executable
+      end
     end
 
     def find(name)
-      executables[name]
+      executables[name] || @fallback_executable
     end
 
     private
 
-    attr_reader :executables
+    attr_reader :executables, :fallback_executable, :fallback_executable_name
   end
 end
