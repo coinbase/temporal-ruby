a2c8267cbb14fa79c0d793288d6b1b5b1ab63b8c
awesomenix <awesomenix@users.noreply.github.com>
2023-06-19 12:00:07 -0700

    Fix task queue type to match enum

--- lib/temporal/connection/grpc.rb
+++ lib/temporal/connection/grpc.rb
@@ -595,7 +595,7 @@ module Temporal
           task_queue: Temporalio::Api::TaskQueue::V1::TaskQueue.new(
             name: task_queue
           ),
-          task_queue_type: Temporalio::Api::Enums::V1::TaskQueueType::Workflow,
+          task_queue_type: Temporalio::Api::Enums::V1::TaskQueueType::TASK_QUEUE_TYPE_WORKFLOW,
           include_task_queue_status: true
         )
         client.describe_task_queue(request)
