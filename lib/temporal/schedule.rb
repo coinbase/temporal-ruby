18d2ce29d4b995e18c3d158b13f57f3638be9ad1
Dhruv Kapadia <dhruv@stripe.com>
2023-12-13 23:56:20 -0500

    Add methods and Schedule module to Temporal obj

---
+++ lib/temporal/schedule.rb
@@ -0,0 +1,16 @@
+require "temporal/schedule/backfill"
+require "temporal/schedule/calendar"
+require "temporal/schedule/describe_schedule_response"
+require "temporal/schedule/interval"
+require "temporal/schedule/list_schedules_response"
+require "temporal/schedule/schedule"
+require "temporal/schedule/schedule_list_entry"
+require "temporal/schedule/schedule_policies"
+require "temporal/schedule/schedule_spec"
+require "temporal/schedule/schedule_state"
+require "temporal/schedule/start_workflow_action"
+
+module Temporal
+  module Schedule
+  end
+end
