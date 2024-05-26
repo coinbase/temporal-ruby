d51fcec89fa899bd55241dcc921172ea7a2ca63c
Jeff Schoner <jeffschoner@gmail.com>
2024-05-26 14:29:30 -0700

    Add example replay test with history file

---
+++ examples/spec/replay/signal_with_start_spec.rb
@@ -0,0 +1,17 @@
+require "workflows/signal_with_start_workflow"
+require "temporal/testing/replay_tester"
+
+describe "signal with start" do
+  let(:replay_tester) { Temporal::Testing::ReplayTester.new }
+
+  it "two misses, one hit, replay, json" do
+    replay_tester.replay_history_json_file(SignalWithStartWorkflow, "spec/replay/histories/signal_with_start.json")
+  end
+
+  it "two misses, one hit, replay, binary" do
+    replay_tester.replay_history_protobuf_file(
+      SignalWithStartWorkflow,
+      "spec/replay/histories/signal_with_start.protobin"
+    )
+  end
+end
--- examples/workflows/signal_with_start_workflow.rb
+++ examples/workflows/signal_with_start_workflow.rb
@@ -8,14 +8,18 @@ class SignalWithStartWorkflow < Temporal::Workflow
 
     workflow.on_signal do |signal, input|
       if signal == expected_signal
+        workflow.logger.info("Accepting expected signal #{signal}: #{input}")
         HelloWorldActivity.execute!('expected signal')
         received = input
+      else
+        workflow.logger.info("Ignoring unexpected signal #{signal}: #{input}")
       end
     end
 
     # Wait for the activity in signal callbacks to complete. The workflow will
     # not automatically wait for any blocking calls made in callbacks to complete
     # before returning.
+    workflow.logger.info("Waiting for expected signal #{expected_signal}")
     workflow.wait_until { received != initial_value }
     received
   end
