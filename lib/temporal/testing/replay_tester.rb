bd22318fd071cc7b593b68f0eb7229821fff3a24
Jeff Schoner <jeffschoner@gmail.com>
2024-06-04 15:05:45 -0700

    Better comments about logging during replay

--- lib/temporal/testing/replay_tester.rb
+++ lib/temporal/testing/replay_tester.rb
@@ -1,10 +1,10 @@
-require "gen/temporal/api/history/v1/message_pb"
-require "json"
-require "temporal/errors"
-require "temporal/metadata/workflow_task"
-require "temporal/middleware/chain"
-require "temporal/workflow/executor"
-require "temporal/workflow/stack_trace_tracker"
+require 'gen/temporal/api/history/v1/message_pb'
+require 'json'
+require 'temporal/errors'
+require 'temporal/metadata/workflow_task'
+require 'temporal/middleware/chain'
+require 'temporal/workflow/executor'
+require 'temporal/workflow/stack_trace_tracker'
 
 module Temporal
   module Testing
@@ -16,7 +16,7 @@ module Temporal
         # Duplicate the configuration so that this doesn't interfere with other tests in the
         # same process that are not replay tests
         @config = config.dup.tap do |c|
-          # Log on replay during replay tests so that more context is available workflow execution
+          # Otherwise, replay tests will produce no logs. This helps with test debugging.
           c.log_on_workflow_replay = true
         end
       end
@@ -52,7 +52,7 @@ module Temporal
       # IMPORTANT: This file is a binary file, not a text file. The protobuf obtained from get_workflow_history_protobuf
       # must be written using binary file options.
       def replay_history_protobuf_file(workflow_class, path)
-        protobuf = File.open(path, "rb") do |f|
+        protobuf = File.open(path, 'rb') do |f|
           f.read
         end
 
@@ -77,12 +77,12 @@ module Temporal
         # than being pulled via a workflow task, no query support, no metrics, and other
         # simplifications. Fake metadata needs to be provided.
         metadata = Temporal::Metadata::WorkflowTask.new(
-          namespace: "replay-test",
+          namespace: 'replay-test',
           id: 1,
-          task_token: "",
+          task_token: '',
           attempt: 1,
-          workflow_run_id: "run_id",
-          workflow_id: "workflow_id",
+          workflow_run_id: 'run_id',
+          workflow_id: 'workflow_id',
           workflow_name: history.find_event_by_id(1).attributes.workflow_type.name
         )
 
@@ -97,23 +97,23 @@ module Temporal
 
         run_result = begin
           executor.run
-        rescue => e
+        rescue StandardError => e
           query = Struct.new(:query_type, :query_args).new(
             Temporal::Workflow::StackTraceTracker::STACK_TRACE_QUERY_NAME,
             nil
           )
           query_result = executor.process_queries(
-            {"stack_trace" => query}
+            { 'stack_trace' => query }
           )
           # Override the stack trace to the point in the workflow code where the failure occured, not the
           # point in the StateManager where non-determinism is detected
-          e.set_backtrace("Fiber backtraces: " + query_result["stack_trace"].result)
+          e.set_backtrace('Fiber backtraces: ' + query_result['stack_trace'].result)
           raise ReplayError, e
         end
 
-        if run_result.commands.any?
-          raise ReplayError, "Workflow task is issuing new commands when it should complete: #{run_result.commands}"
-        end
+        return unless run_result.commands.any?
+
+        raise ReplayError, "Workflow task is issuing new commands when it should complete: #{run_result.commands}"
       end
 
       # Protobuf does not define a consistent format for enums in JSON. In some Temporal SDKs, the event
@@ -126,9 +126,9 @@ module Temporal
       # readable form on output.
       def self.correct_event_types(text, pretty_print: true)
         json_hash = ::JSON.parse(text)
-        json_hash["events"].each do |event|
-          if !event["eventType"].start_with?("EVENT_TYPE")
-            event["eventType"] = "EVENT_TYPE_" + event["eventType"].gsub(/(.)([A-Z])/, "\\1_\\2").upcase
+        json_hash['events'].each do |event|
+          unless event['eventType'].start_with?('EVENT_TYPE')
+            event['eventType'] = 'EVENT_TYPE_' + event['eventType'].gsub(/(.)([A-Z])/, '\\1_\\2').upcase
           end
         end
 
