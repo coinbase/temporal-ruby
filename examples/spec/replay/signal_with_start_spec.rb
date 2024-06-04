a8f87faf488ca8d556dd82f2fd083db476c00a80
Jeff Schoner <jeffschoner@gmail.com>
2024-06-04 15:03:55 -0700

    Use binpb extension for protobuf biniaries

--- examples/spec/replay/signal_with_start_spec.rb
+++ examples/spec/replay/signal_with_start_spec.rb
@@ -1,17 +1,17 @@
-require "workflows/signal_with_start_workflow"
-require "temporal/testing/replay_tester"
+require 'workflows/signal_with_start_workflow'
+require 'temporal/testing/replay_tester'
 
-describe "signal with start" do
+describe 'signal with start' do
   let(:replay_tester) { Temporal::Testing::ReplayTester.new }
 
-  it "two misses, one hit, replay, json" do
-    replay_tester.replay_history_json_file(SignalWithStartWorkflow, "spec/replay/histories/signal_with_start.json")
+  it 'two misses, one hit, replay, json' do
+    replay_tester.replay_history_json_file(SignalWithStartWorkflow, 'spec/replay/histories/signal_with_start.json')
   end
 
-  it "two misses, one hit, replay, binary" do
+  it 'two misses, one hit, replay, binary' do
     replay_tester.replay_history_protobuf_file(
       SignalWithStartWorkflow,
-      "spec/replay/histories/signal_with_start.protobin"
+      'spec/replay/histories/signal_with_start.binpb'
     )
   end
 end
