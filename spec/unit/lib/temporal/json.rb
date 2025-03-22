51c0e4d7982601023340daa127e7f1f01a62cc29
Semyon Pupkov <mail@semyonpupkov.com>
2025-03-22 13:40:59 +0500

    Fix serialize/deserialize circular entries
    Fixes: #204

--- spec/unit/lib/temporal/json.rb
+++ spec/unit/lib/temporal/json.rb
@@ -2,11 +2,24 @@ require 'temporal/json'
 
 describe Temporal::JSON do
   let(:hash) { { 'one' => 'one', two: :two, ':three' => ':three' } }
-  let(:json) { '{"one":"one",":two":":two","\u003athree":"\u003athree"}' }
+  let(:json) { '{"^i":1,"one":"one",":two":":two","\u003athree":"\u003athree"}' }
 
   describe '.serialize' do
     it 'generates JSON string' do
       expect(described_class.serialize(hash)).to eq(json)
+    end
+
+    it 'does not raise error on circular entries' do
+      author = Struct.new('Author', :name, :books)
+      book = Struct.new('Book', :author)
+      author_entry = author.new(name: 'David')
+      book_entry = book.new(title: 'test', author: author)
+
+      author_entry.books = [book_entry]
+      json = described_class.serialize(author_entry)
+
+      result = described_class.deserialize(json)
+      expect(result).to eq(author_entry)
     end
   end
 
