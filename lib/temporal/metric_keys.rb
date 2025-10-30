e9e55590cc32593f01e9be8711e796ddb0bc49c9
Kostiantyn Masliuk <1pkg@protonmail.com>
2025-10-30 14:35:21 -0700

    capture poller poll latency metric

--- lib/temporal/metric_keys.rb
+++ lib/temporal/metric_keys.rb
@@ -2,11 +2,13 @@ module Temporal
   module MetricKeys
     ACTIVITY_POLLER_TIME_SINCE_LAST_POLL = 'activity_poller.time_since_last_poll'.freeze
     ACTIVITY_POLLER_POLL_COMPLETED = 'activity_poller.poll_completed'.freeze
+    ACTIVITY_POLLER_POLL_LATENCY = 'activity_poller.poll_latency'.freeze
     ACTIVITY_TASK_QUEUE_TIME = 'activity_task.queue_time'.freeze
     ACTIVITY_TASK_LATENCY = 'activity_task.latency'.freeze
 
     WORKFLOW_POLLER_TIME_SINCE_LAST_POLL = 'workflow_poller.time_since_last_poll'.freeze
     WORKFLOW_POLLER_POLL_COMPLETED = 'workflow_poller.poll_completed'.freeze
+    WORKFLOW_POLLER_POLL_LATENCY = 'worfklow_poller.poll_latency'.freeze
     WORKFLOW_TASK_QUEUE_TIME = 'workflow_task.queue_time'.freeze
     WORKFLOW_TASK_LATENCY = 'workflow_task.latency'.freeze
     WORKFLOW_TASK_EXECUTION_FAILED = 'workflow_task.execution_failed'.freeze
